379321ab7e09f23a3048e8fe0277efb0538f47ef
PA187 - Elder Vivot Dias <PA187@soma.com>
2019-12-06 18:29:38 -0300

    Implementado melhorias na busca das notas e fornecedor pra comparacao com o banco de dados

--- backend/accounting_integration/src/services/rules/ComparePaymentsAndProofWithExtracts.py
+++ backend/accounting_integration/src/services/rules/ComparePaymentsAndProofWithExtracts.py
@@ -34,6 +34,7 @@ class ComparePaymentsAndProofWithExtracts(object):
         dayAfter = None
         dayBefore = None
 
+        # procura nos dias positivos (da data atual até mais 3 dias pra frente)
         for day in range(0, self._numberOfDaysInterval["daysAfter"]+1):
             paymentDateComparison = funcoesUteis.transformaCampoDataParaFormatoBrasileiro(paymentDate + timedelta(days=day))
             if extract['dateTransaction'] == paymentDateComparison and extract['amount'] == amountPaid and extract['operation'] == operation \
@@ -47,10 +48,12 @@ class ComparePaymentsAndProofWithExtracts(object):
             if extract['dateTransaction'] == paymentDateComparison and extract['amount'] == amountPaid and extract['operation'] == operation:
                 dayAfter = day
                 break
-
+        
+        # se dayAfter é igual a zero quer dizer que o pagamento está no mesmo dia
         if dayAfter == 0:
             return dayAfter
 
+        # procura nos dias negativo (da data atual até menos 3 dias pra trás)
         for day in range(0, self._numberOfDaysInterval["daysBefore"]+1):
             paymentDateComparison = funcoesUteis.transformaCampoDataParaFormatoBrasileiro(paymentDate + timedelta(days=day))
             if extract['dateTransaction'] == paymentDateComparison and extract['amount'] == amountPaid and extract['operation'] == operation \
@@ -65,6 +68,7 @@ class ComparePaymentsAndProofWithExtracts(object):
                 dayBefore = day
                 break
 
+        # senão encontrar nenhum dia retorna nulto
         if dayAfter is None and dayBefore is None:
             return None
         else:
@@ -93,6 +97,7 @@ class ComparePaymentsAndProofWithExtracts(object):
 
     def comparePaymentsWithProof(self):
         for key, payment in enumerate(self._payments):
+            # se for um pagamento que já leu no comprovante pula ele
             if self._paymentsAlreadyRead.count(key) > 0:
                 continue
             else:
@@ -101,6 +106,7 @@ class ComparePaymentsAndProofWithExtracts(object):
 
     # This the end process, he comes the others
     def comparePaymentsFinalWithExtract(self):
+        # chama a execução das funções pra não ter que chamá-las duas vezes no main
         self.compareProofWithPayments()
         self.comparePaymentsWithProof()
 
--- backend/accounting_integration/src/services/rules/ComparePaymentsFinalWithDataBase.py
+++ backend/accounting_integration/src/services/rules/ComparePaymentsFinalWithDataBase.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.join(fileDir, 'backend'))
 sys.path.append(os.path.join(fileDir, 'backend/accounting_integration/src/services'))
 
 import json
+from math import floor
 from datetime import datetime, timedelta
 from tools.leArquivos import leXls_Xlsx, leTxt, readJson
 import tools.funcoesUteis as funcoesUteis
@@ -16,11 +17,45 @@ from read_files.ExtractsOFX import ExtractsOFX
 
 class ComparePaymentsFinalWithDataBase(object):
     
-    def __init__(self, providers, entryNotes=[]):
+    def __init__(self, providers=[], entryNotes=[], payments=[]):
         self._providers = providers
         self._entryNotes = entryNotes
-        self._listWordsNotConsiderInTheName = ['LTDA', '-', 'ME', 'EPP', 'EIRELI', 'MEI', 'EI', 'S.A.', 'SA', 'S.A']
+        self._payments = payments
+        self._listWordsNotConsiderInTheName = ['LTDA', 'LTDA.', '-', 'ME', 'ME.', 'EPP', 'EPP.', 'EIRELI', 'EIRELI.', \
+            'MEI', 'MEI.', 'EI', 'EI.', 'S.A.', 'SA', 'S.A', 'S/A']
+        self._listWordsAbbreviatedToChange = {
+            "COM.": "COMERCIO",
+            "PROD.": "PRODUCAO",
+            "IND.": "INDUSTRIA",
+            "IMP.": "IMPORTACAO",
+            "EXP.": "EXPORTACAO",
+            "MAT.": "MATERIAIS",
+            "LIMP.": "LIMPEZA",
+            "SERV.": "SERVICO",
+            "EQUIP.": "EQUIPAMENTOS",
+            "P/": "PARA",
+            "ARTIG.": "ARTIGOS",
+            "DIST.": "DISTRIBUIDORA",
+            "CONST.": "CONSTRUCAO"
+        }
 
+    def changeAbbreviatedWord(self, name):
+        nameChanged = ""
+        for wordName in name.split():
+            def chechWordIsInTheList():
+                for abbreviate, wordToChange in self._listWordsAbbreviatedToChange.items():
+                    if wordName == abbreviate:
+                        return wordToChange
+            
+            wordNew = chechWordIsInTheList()
+            if wordNew is not None:
+                nameChanged = f"{nameChanged} {wordNew}"
+            else:
+                nameChanged = f"{nameChanged} {wordName}"
+        nameChanged = funcoesUteis.minimalizeSpaces(nameChanged)
+
+        return nameChanged
+    
     def removeWordsThatAreNotNames(self, name):
         nameChanged = ""
         for wordName in name.split():
@@ -29,27 +64,32 @@ class ComparePaymentsFinalWithDataBase(object):
                     if wordName == listWordNotConsider:
                         return True
             
+            # adiciona apenas as palavras que não estão na lista
             if chechWordIsInTheList() is None:
                 nameChanged = f"{nameChanged} {wordName}"
         nameChanged = funcoesUteis.minimalizeSpaces(nameChanged)
 
-        return nameChanged       
+        return nameChanged
 
-    def returnDataProvider(self, codi_for=0, cgce='', name=''):
+    def returnDataProvider(self, codi_for=0, cgce=None, name=None, degreeOfReliability=0):
 
         for provider in self._providers:
 
-            if provider['codi_for'] == codi_for:
+            if int(provider['codi_for']) == int(codi_for):
                 return provider
 
-            if provider['cgce_for'] == cgce:
+            if provider['cgce_for'] == cgce and cgce is not None:
                 return provider
 
-            nameProvider = self.removeWordsThatAreNotNames(provider["nome_for"])
-            nameArgument = self.removeWordsThatAreNotNames(name)
+            # agora as verificações pelo nome (esta primeira tem grau alto de confiabilidade)
+            if name is None:
+                continue
+
+            nameProvider = self.changeAbbreviatedWord(self.removeWordsThatAreNotNames(provider["nome_for"]))
+            nameArgument = self.changeAbbreviatedWord(self.removeWordsThatAreNotNames(name))
 
             countNameProvider = len(nameProvider.split())
-     
+
             def findProviderForName():
                 countEqualWords = 0
                 nameArgumentSplit = nameArgument.split(' ')
@@ -61,39 +101,68 @@ class ComparePaymentsFinalWithDataBase(object):
                     return provider
                 
                 if countNameProvider >= 3:
-                    if countNameProvider <= float(countEqualWords):
+                    if floor(countNameProvider * 0.7) <= countEqualWords:
                         return provider
-                
-                
+                else:
+                    if nameProvider.count(nameArgument[0:floor(len(nameArgument)*0.75)]) > 0:
+                        return provider
+
+                # grau de confibilidade mais baixo, só deve ser usado junto com outra confirmação, como número da nota por exemplo
+                if degreeOfReliability == 1:
+                    if len(nameArgument) >= 10:
+                        if nameProvider.count(nameArgument[0:floor(len(nameArgument)*0.30)]) > 0:
+                            return provider
+                    else:
+                        if nameProvider.count(nameArgument[0:floor(len(nameArgument)*0.60)]) > 0:
+                            return provider
 
             if findProviderForName() is not None:
                 return provider
 
-    def returnDataEntryNote(self, note=0, nameProvider='', cgceProvider='', ddoc_ent = None, dent_ent = None, amountPayment = 0.0):
+    def returnDataEntryNote(self, note=0, cgceProvider=None, ddoc_ent = None, dent_ent = None, amountPayment = 0.0, nameProvider=None):
 
         for entryNote in self._entryNotes:
             provider = self.returnDataProvider(entryNote["codi_for"])
+            # print(provider)
+                    
+            # utilizo uma função pois foi precisar comparar o cgce duas vezes, então chamo ele nos dois casos diferentes
+            def returnNote(cgceProviderSearch):
+                if int(entryNote['nume_ent']) == note and cgceProviderSearch == cgceProvider:
+                    return entryNote
+
+                if int(entryNote['nume_ent']) == note and entryNote["ddoc_ent"] == ddoc_ent:
+                    return entryNote
+
+                if int(entryNote['nume_ent']) == note and entryNote["dent_ent"] == dent_ent:
+                    return entryNote
+
+                if int(entryNote['nume_ent']) == note and float(entryNote["vcon_ent"]) == amountPayment:
+                    return entryNote
+
+                if entryNote['ddoc_ent'] == ddoc_ent and cgceProviderSearch == cgceProvider:
+                    return entryNote
+
+                if entryNote["dent_ent"] == dent_ent and cgceProviderSearch == cgceProvider:
+                    return entryNote
+                
+                if float(entryNote["vcon_ent"]) == amountPayment and cgceProviderSearch == cgceProvider:
+                    return entryNote
+
+            cgce_for = funcoesUteis.analyzeIfFieldIsValid(provider, "cgce_for", None)
+            if returnNote(cgce_for) is not None:
+                return returnNote(cgce_for)
+
+            # busca a nota olhando pelo nome da empresa
+            providerByName = self.returnDataProvider(name=nameProvider, degreeOfReliability=1)
+            cgce_for = funcoesUteis.analyzeIfFieldIsValid(providerByName, "cgce_for", None)
+            # print(cgce_for)
+            print(cgce_for, entryNote["nume_ent"])
+            if returnNote(cgce_for) is not None:
+                return returnNote(cgce_for)
+
+    # def processComparateWithDataBase(self):
+    #     for payment in self._payments:
             
-            if int(entryNote['nume_ent']) == note and provider["cgce_for"] == cgceProvider:
-                return entryNote
-
-            if int(entryNote['nume_ent']) == note and entryNote["ddoc_ent"] == ddoc_ent:
-                return entryNote
-
-            if int(entryNote['nume_ent']) == note and entryNote["dent_ent"] == dent_ent:
-                return entryNote
-
-            if int(entryNote['nume_ent']) == note and float(entryNote["vcon_ent"]) == amountPayment:
-                return entryNote
-
-            if entryNote['ddoc_ent'] == ddoc_ent and provider["cgce_for"] == cgceProvider:
-                return entryNote
-
-            if entryNote["dent_ent"] == dent_ent and provider["cgce_for"] == cgceProvider:
-                return entryNote
-            
-            if float(entryNote["vcon_ent"]) == amountPayment and provider["cgce_for"] == cgceProvider:
-                return entryNote
 
 
 if __name__ == "__main__":
@@ -102,6 +171,8 @@ if __name__ == "__main__":
     # print(entryNotes)
     comparePaymentsFinalWithDataBase = ComparePaymentsFinalWithDataBase(providers, entryNotes)
     # comparePaymentsFinalWithDataBase.removeWordsThatAreNotNames(name='LEXUS ENGENHARIA LTDA')
-    print(comparePaymentsFinalWithDataBase.returnDataProvider(name='LEXUS ENGENHARIA'))
+    # print(comparePaymentsFinalWithDataBase.changeAbbreviatedWord('P/ SERVICO'))
+    # print(comparePaymentsFinalWithDataBase.returnDataProvider(name='LEXUS ENGENHARIA'))
     # print(comparePaymentsFinalWithDataBase.returnDataProvider(5, "07939369000103"))
-    # print(comparePaymentsFinalWithDataBase.returnDataEntryNote(164, '', "07939369000103"))
+    # print(comparePaymentsFinalWithDataBase.returnDataProvider(5))
+    print(comparePaymentsFinalWithDataBase.returnDataEntryNote(164, nameProvider='LEXUS ENGENHARIA'))
