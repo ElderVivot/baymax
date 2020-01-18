a68a3566e7c5953f28666694269787e4241a3c13
= <=>
2020-01-18 14:29:18 -0300

    Implementado a exportacao de NF-e de entrada de acordo a nova estrutura (separar por empresa em cada competencia)

--- backend/extract/src/fiscal/efentradas.py
+++ backend/extract/src/fiscal/efentradas.py
@@ -11,9 +11,10 @@ sys.path.append(os.path.join(fileDir, 'backend'))
 import pandas as pd
 import pyodbc
 import json
+import datetime
 from db.ConexaoBanco import DB
 from tools.leArquivos import readJson
-# from functions.usefulFunctions import parseTypeFiedValueCorrect
+import functions.extractFunctions as extractFunctions
 
 wayToSaveFiles = open(os.path.join(fileDir, 'backend/extract/src/WayToSaveFiles.json') )
 wayDefault = json.load(wayToSaveFiles)
@@ -29,16 +30,44 @@ class extractEfentradas():
         self._baseWayToSave = os.path.join(wayDefault['wayDefaultToSaveFiles'], 'entradas')
         if os.path.exists(self._baseWayToSave) is False:
             os.makedirs(self._baseWayToSave)
+        self._today = datetime.date.today()
+        self._currentMonth = self._today.month
+        self._currentYear = self._today.year
 
-    def exportData(self, filterCompanie=0, filterMonthStart=1, filterYearStart=2013):
+    def exportData(self, filterCompanie=0, filterMonthStart=1, filterYearStart=2013, filterMonthEnd=0, filterYearEnd=0):
+        filterMonthEnd = self._currentMonth if filterMonthEnd == 0 else filterMonthEnd
+        filterYearEnd = self._currentYear if filterYearEnd == 0 else filterYearEnd
+        
         try:
             for companie in self._dataCompanies:
-                self._wayToSave = os.path.join(self._baseWayToSave, f"{companie['codi_emp']}-efentradas.json")
-                    
-                # only companies actives
-                if companie['stat_emp'] not in ('I') and companie['dina_emp'] is None:
-                    if companie['codi_emp'] == filterCompanie or filterCompanie == 0:
-                        print(f"- Exportando NF de entradas da empresa {companie['codi_emp']} - {companie['nome_emp']}")
+                codi_emp = companie['codi_emp']
+
+                if filterCompanie != 0 and filterCompanie != codi_emp:
+                    continue # ignora as empresas que não estão no filtro
+
+                print(f"- Exportando NF de entradas da empresa {codi_emp} - {companie['nome_emp']}")
+                
+                wayToSaveCompanie = os.path.join(self._baseWayToSave, str(codi_emp))
+                if os.path.exists(wayToSaveCompanie) is False:
+                    os.makedirs(wayToSaveCompanie)
+                        
+                competenceStartEnd = extractFunctions.returnCompetenceStartEnd(companie, filterMonthStart, filterYearStart, filterMonthEnd, filterYearEnd)
+                startMonth = competenceStartEnd['filterMonthStart']
+                startYear = competenceStartEnd['filterYearStart']
+                endMonth = competenceStartEnd['filterMonthEnd']
+                endYear = competenceStartEnd['filterYearEnd']
+
+                year = startYear
+
+                while year <= endYear:
+
+                    months = extractFunctions.returnMonthsOfYear(year, startMonth, startYear, endMonth, endYear)
+
+                    print('\t - ', end='')
+                    for month in months:
+                        print(f'{month:0>2}/{year}, ', end='')
+
+                        self._wayToSave = os.path.join(wayToSaveCompanie, f'{str(year)}{month:0>2}.json')
                         self._cursor = self._connection.cursor()
                         sql = ( f"SELECT ent.codi_emp, ent.codi_ent, ent.nume_ent, ent.codi_for, forn.nome_for, ent.codi_esp, ent.codi_acu, ent.codi_nat, ent.segi_ent, "
                                 f"       ent.seri_ent, ent.dent_ent, ent.ddoc_ent, ent.vcon_ent "
@@ -46,15 +75,18 @@ class extractEfentradas():
                                 f"       INNER JOIN bethadba.effornece AS forn "
                                 f"            ON    forn.codi_emp = ent.codi_emp "
                                 f"              AND forn.codi_for = ent.codi_for "
-                                f" WHERE ent.codi_emp = {companie['codi_emp']}"
-                                f"   AND year(ent.dent_ent) >= {filterYearStart}"
-                                f"   AND month(ent.dent_ent) >= {filterMonthStart}"
+                                f" WHERE ent.codi_emp = {codi_emp}"
+                                f"   AND year(ent.dent_ent) = {year}"
+                                f"   AND month(ent.dent_ent) = {month}"
                                 f"ORDER BY ent.codi_emp, ent.dent_ent DESC, ent.codi_ent")
                         self._cursor.execute(sql)
 
                         df = pd.read_sql_query(sql, self._connection)
 
                         df.to_json(self._wayToSave, orient='records', date_format='iso' ) 
+
+                    print('')
+                    year += 1
         except Exception as e:
             print(f"Erro ao executar a consulta. O erro é: {e}")
         finally:
