cec3509702677d1cb985a4b86a7a7b24cb501f03
= <=>
2020-03-27 18:01:27 -0300

    Melhorado no ComparePaymentsAndProof a comparação entre o comprovante de pagamento e financeiro do cliente.

--- backend/accounting_integration/src/services/rules/ComparePaymentsAndProofWithExtracts.py
+++ backend/accounting_integration/src/services/rules/ComparePaymentsAndProofWithExtracts.py
@@ -23,52 +23,89 @@ class ComparePaymentsAndProofWithExtracts(object):
         self._paymentsAlreadyRead = []
         self._extractsExistsInPayments = [] # este daqui são para os extratos que encontrou correlação nos pagamentos, na planilha de extratos conterá um campo com esta informação
         self._extractsToSearch = self._extracts.copy() # este daqui é que vai ser utilizado pra procurar se o pagamento existe correspondente no extrato, conforme for encontrando vai excluindo pra não procurar novamente num que já tinha retornado
+        self._paymentsToSearch = self._payments.copy() # este daqui é que vai ser utilizado pra procurar se o pagamento existe correspondente no comprovante de pagamento, conforme for encontrando vai excluindo pra não procurar novamente num que já tinha retornado
         self._extractsFinal = []
         self._numberOfDaysInterval = 3
+        self._numberOfDaysIntervalPaymentWithProof = 3
         self._financyIsReliable = funcoesUteis.returnDataFieldInDict(self._settings, ['financy', 'isReliable'], True)
 
-    def returnDayFoundInPayment(self, payment, paymentDate, amountPaid):
-        paymentDate = paymentDate
-        dayAfter = None
-        dayBefore = None
+    def findsLessDifferenceBetweenDatesInArrayOfObject(self, arrayObjects, nameFieldOfDate, baseDate):
+        objReturn = None
+        smallerDifferenceBetweenDates = None
 
-        # olha até 10 dias a mais
-        for day in range(0, 3):
+        for obj in arrayObjects:
+            objDate = obj[nameFieldOfDate]
+
+            differenceBetweenDates = abs((baseDate - objDate).days)
+
+            # se a diferença entre datas for zero, nem segue a pesquisa, pois já retorno a data correta
+            if differenceBetweenDates == 0:
+                objReturn = obj
+                break
+
+            if smallerDifferenceBetweenDates is None:
+                smallerDifferenceBetweenDates = differenceBetweenDates
+                objReturn = obj
+
+            if differenceBetweenDates < smallerDifferenceBetweenDates:
+                smallerDifferenceBetweenDates = differenceBetweenDates
+                objReturn = obj
+
+        return objReturn
+
+    def returnDayFoundInPayment(self, payment, paymentDate, amountPaid, bank, account, typeComparation):
+        # :typeComparation --> 1 = completa (banco e conta corrente); 2 = média (banco); 3 = fraca (apenas valores, data e operação)
+        datePayment = payment['paymentDate']
+        amountPayment = payment['amountPaid']
+        bankPayment = payment['bank']
+        accountPayment = payment['account']
+        search = False
+
+        # caso o valor seja diferente nem processa os dias
+        if amountPayment != amountPaid:
+            return search
+
+        # caso a diferença entre as datas seja maior que o intervalo definido já retorna falso
+        differenceBetweenDates = abs((paymentDate - datePayment).days)
+        if differenceBetweenDates > self._numberOfDaysIntervalPaymentWithProof:
+            return search
+
+        # procura nos dias positivos e negativos (da data atual até mais 3 dias pra frente/atrás)
+        for day in range(0, self._numberOfDaysIntervalPaymentWithProof+1):
             paymentDateComparison = paymentDate + timedelta(days=day)
-            if payment['paymentDate'] == paymentDateComparison and payment['amountPaid'] == amountPaid:
-                dayAfter = day
-                break
-        
-        # se dayAfter é igual a zero quer dizer que o pagamento está no mesmo dia
-        if dayAfter == 0:
-            return dayAfter
+            paymentDateComparisonLess = paymentDate + timedelta(days=-day)
+            if typeComparation == 1:
+                if ( datePayment == paymentDateComparison or datePayment == paymentDateComparisonLess ) and amountPayment == amountPaid and bankPayment.find(bank) >= 0 and accountPayment.find(account) >= 0:
+                    search = True
+                    break
+            elif typeComparation == 2:
+                if ( datePayment == paymentDateComparison or datePayment == paymentDateComparisonLess ) and amountPayment == amountPaid and bankPayment.find(bank) >= 0:
+                    search = True
+                    break
+            elif typeComparation == 3:
+                if ( datePayment == paymentDateComparison or datePayment == paymentDateComparisonLess ) and amountPayment == amountPaid:
+                    search = True
+                    break
 
-        # olha até 10 dias a menos
-        for day in range(1, 3):
-            paymentDateComparison = paymentDate + timedelta(days=-day)
-            if payment['paymentDate'] == paymentDateComparison and payment['amountPaid'] == amountPaid:
-                dayBefore = day
-                break
+        return search        
 
-        # senão encontrar nenhum dia retorna nulo
-        if dayAfter is None and dayBefore is None:
-            return None
-        else:
-            if dayAfter is None and dayBefore is not None:
-                return dayBefore
-            if dayAfter is not None and dayBefore is None:
-                return dayAfter
-                
-            if dayAfter < dayBefore:
-                return dayAfter
-            else:
-                return dayBefore
+    def returnDataPayment(self, paymentDate, amountPaid, bank='', account=''):  
+        # o range de 1 a 3 é pq primeiro vou rodar o typeComparation com mais confiabilidade (igual a 1), depois rodo com média e fraca por último
+        for numberSequencial in range(1, 4):
+            paymentsFound = []
+            paymentReturn = None
 
-    def returnDataPayment(self, paymentDate, amountPaid):
-        for key, payment in enumerate(self._payments):
-            day = self.returnDayFoundInPayment(payment, paymentDate, amountPaid)
-            if day is not None:
-                return [payment, key] # o key eu retorno pra depois identificar quais pagamentos já foram processados nas provas de pagamentos e não imprimi-los novamente
+            for payment in self._paymentsToSearch:
+                search = self.returnDayFoundInPayment(payment, paymentDate, amountPaid, bank, account, typeComparation=numberSequencial)
+                if search is True:
+                    paymentsFound.append(payment)
+            
+            if len(paymentsFound) > 0:
+                paymentReturn = self.findsLessDifferenceBetweenDatesInArrayOfObject(paymentsFound, 'paymentDate', paymentDate)
+                if paymentReturn is not None:
+                    self._paymentsAlreadyRead.append(paymentReturn)
+                    self._paymentsToSearch.remove(paymentReturn)
+                    return paymentReturn
 
     def returnDayFoundInExtract(self, extract, paymentDate, amountPaid, operation, bank, account, typeComparation):
         # :typeComparation --> 1 = completa (banco e conta corrente); 2 = média (banco); 3 = fraca (apenas valores, data e operação)
@@ -163,29 +200,25 @@ class ComparePaymentsAndProofWithExtracts(object):
 
     def compareProofWithPayments(self):
         for proof in self._proofOfPayments:
-            paymentArray = self.returnDataPayment(proof["paymentDate"], proof["amountPaid"])
-            try:
-                payment = paymentArray[0]
+            paymentDate = funcoesUteis.analyzeIfFieldIsValid(proof, 'paymentDate')
+            amountPaid = funcoesUteis.analyzeIfFieldIsValid(proof, 'paymentDate')
+            bank = funcoesUteis.analyzeIfFieldIsValid(proof, 'bank')
+            account = funcoesUteis.analyzeIfFieldIsValid(proof, 'account')
 
-                self._paymentsAlreadyRead.append(paymentArray[1])
-            except Exception:
-                payment = []
-            
-            proof["foundProof"] = True
-            proof["document"] = funcoesUteis.analyzeIfFieldIsValid(payment, "document")
-            proof["historic"] = funcoesUteis.analyzeIfFieldIsValid(payment, "historic")
-            proof["amountDiscount"] = funcoesUteis.analyzeIfFieldIsValid(payment, "amountDiscount")
-            proof["amountInterest"] = funcoesUteis.analyzeIfFieldIsValid(payment, "amountInterest")
-            proof["amountOriginal"] = funcoesUteis.analyzeIfFieldIsValid(payment, "amountOriginal")
-            proof["accountPlan"] = funcoesUteis.analyzeIfFieldIsValid(payment, "accountPlan")
-            proof["bankCheck"] = funcoesUteis.analyzeIfFieldIsValid(payment, "bankCheck")
+            payment = self.returnDataPayment(paymentDate, amountPaid, bank, account)
+
+            if payment is not None:
+                for nameField, valueField in payment.items():
+                    if funcoesUteis.analyzeIfFieldIsValid(proof, nameField, None) is None:
+                        proof[nameField] = valueField
 
             self._paymentsFinal.append(proof)
 
     def comparePaymentsWithProof(self):
-        for key, payment in enumerate(self._payments):
-            # se for um pagamento que já leu no comprovante pula ele
-            if self._paymentsAlreadyRead.count(key) > 0:
+        for payment in self._payments:
+            existPaymentsInProofOfPayments = list(filter(lambda x: x == payment, self._paymentsAlreadyRead))
+
+            if len(existPaymentsInProofOfPayments) > 0:
                 continue
             else:
                 payment["foundProof"] = False
@@ -246,6 +279,7 @@ class ComparePaymentsAndProofWithExtracts(object):
 
         return self._paymentsFinal
 
+    # esta função é a responsável por inserir a informação se aquele valor do extrato também existe no pagamento ou não
     def analyseIfExtractIsInPayment(self):
         for extract in self._extracts:
             existProofInPayments = list(filter(lambda x: x == extract, self._extractsExistsInPayments))
