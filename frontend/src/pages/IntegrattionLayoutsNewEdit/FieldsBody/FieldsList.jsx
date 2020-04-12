bac1647ecab0eb5852d9cfb0edf66e205fa7bf76
= <=>
2020-04-12 09:58:52 -0300

    Implementado o FieldsList de acordo nova estrutura

--- frontend/src/pages/IntegrattionLayoutsNewEdit/FieldsBody/FieldsList.jsx
+++ frontend/src/pages/IntegrattionLayoutsNewEdit/FieldsBody/FieldsList.jsx
@@ -2,87 +2,75 @@ import React from 'react'
 
 import IntegrattionLayoutsFieldsNewOrEdit from './FieldsNewOrEdit'
 
-const fieldsOptions = [
-    { value: 'paymentDate', label: 'Data de Pagamento'},
-    { value: 'document', label: 'NF ou Documento'},
-    { value: 'cgceProvider', label: 'CNPJ Fornedor'},
-    { value: 'nameProvider', label: 'Nome Fornecedor'},
-    { value: 'bank', label: 'Banco/Caixa'},
-    { value: 'account', label: 'Conta Corrente'},
-    { value: 'amountPaid', label: 'Valor Pago'},
-    { value: 'amountOriginal', label: 'Valor Original'},
-    { value: 'amountInterest', label: 'Valor Juros'},
-    { value: 'amountFine', label: 'Valor Multa'},
-    { value: 'amountDiscount', label: 'Valor Desconto'},
-    { value: 'amountDevolution', label: 'Valor Devolução'},
-    { value: 'dueDate', label: 'Data de Vencimento'},
-    { value: 'issueDate', label: 'Data de Emissão'},
-    { value: 'historic', label: 'Histórico'},
-    { value: 'category', label: 'Categoria'},
-    { value: 'accountPlan', label: 'Plano de Contas'},
-    { value: 'parcelNumber', label: 'Número da Parcela'},
-    { value: 'companyBranch', label: 'Filial/Empresa'},
-    { value: 'typeMoviment', label: 'Tipo Movimento'},
-]
+function IntegrattionLayoutsFieldsList( { values, errors, touched, handleChange, handleBlur, setFieldValue, setFieldTouched, defaultValues, initialValues } ){
 
-function IntegrattionLayoutsFieldsList( { idx, fieldsFile, setFieldValue, initialValues, fileType, valuesParent } ){
-
-    const deleteField = () => {
-        const updatedfieldsFile = [...fieldsFile]
-        updatedfieldsFile.splice(idx, 1)
-        setFieldValue("fields", updatedfieldsFile)
-    }
-
-    const EditField = () => {
-        return (
-            < IntegrattionLayoutsFieldsNewOrEdit
-                key={`fieldFile-${idx}`}
-                idx={idx}
-                setFieldValueParent={setFieldValue}
-                fieldsOptions={fieldsOptions}
-                initialValues={initialValues[idx]}
-                fileType={fileType}
-                valuesParent={valuesParent}
-            />
-        )
-    }
-
-    function ButtonDelete(isDisabled=false){
-        return( 
-            <button className="btn btn-danger ml-2 btn-sm btn10px" type="button" 
-                onClick={deleteField} disabled={isDisabled}>
-                <i className="fa fa-trash"></i>
-            </button>
-        )
-    }
-
-    function Buttons(){
-        return (
-            <div>
-                {EditField()}
-                {ButtonDelete()}
-            </div>
-        )
-    }
-
-    const nameFieldVector = fieldsOptions.filter(option => option.value === fieldsFile[idx].nameField)
-    let nameField = ""
-    if(nameFieldVector.length > 0){
-        nameField = nameFieldVector[0].label
-    }
-    
     return (
         <>
-            <tr className="d-flex justify-content-center text-center">
-                <td className="col-4 align-center">{nameField}</td>
-                <td className="col-1 align-center">{fieldsFile[idx].positionInFile}</td>
-                <td className="col-1 align-center">{fieldsFile[idx].positionInFileEnd}</td>
-                <td className="col-3 align-center">{fieldsFile[idx].nameColumn}</td>
-                <td className="col-1 align-center">{fieldsFile[idx].formatDate}</td>
-                <td className="col-2 align-center">
-                    <Buttons />
-                </td>
-            </tr>
+            <div className="form row mt-2">
+                <label className="col-form-label font-weight-600">Configuração dos Campos do Layout:</label>                                
+            </div>
+
+            <div className="form row">
+                <table className="table ml-3 table-striped table-bordered table-hover">
+                    <thead>
+                        <tr className="d-flex justify-content-center text-center">
+                            <th className="col-4 fields-of-table align-center">Campo</th>
+                            <th className="col-1 fields-of-table align-center">Posição Inicial</th>
+                            <th className="col-1 fields-of-table align-center">Posição Final</th>
+                            <th className="col-3 fields-of-table align-center">Nome Coluna</th>
+                            <th className="col-1 fields-of-table align-center">Formato Data</th>
+                            <th className="col-2 fields-of-table align-center">
+                                <div className="font-weight-600">Ações</div>
+                                <button className="btn btn-success btn-sm btn10px ml-3" type="button" style={{height:25}}
+                                    onClick={() => {
+                                        setFieldValue("fields", [...values.fields, defaultValues.fields[0]])
+                                    } }>
+                                    <i className="fa fa-plus"></i>
+                                </button>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>{
+                        values.fields.map( (field, idx) => (
+                            <tr key={`values.fields[${idx}]`} className="d-flex justify-content-center text-center">
+                                <td className="col-4 align-center">{values.fields[idx].nameField}</td>
+                                <td className="col-1 align-center">{values.fields[idx].positionInFile}</td>
+                                <td className="col-1 align-center">{values.fields[idx].positionInFileEnd}</td>
+                                <td className="col-3 align-center">{values.fields[idx].nameColumn}</td>
+                                <td className="col-1 align-center">{values.fields[idx].formatDate}</td>
+                                <td className="col-2 align-center">
+                                    <div>
+                                        < IntegrattionLayoutsFieldsNewOrEdit
+                                            key={`values.fields[${idx}]`}
+                                            idx={idx}
+                                            values={values}
+                                            errors={errors}
+                                            touched={touched}
+                                            handleChange={handleChange}
+                                            handleBlur={handleBlur}
+                                            setFieldValue={setFieldValue}
+                                            setFieldTouched={setFieldTouched}
+                                            initialValues={initialValues}
+                                        />
+
+                                        <button className="btn btn-danger ml-2 btn-sm btn10px" type="button"
+                                            onClick={() => {
+                                                const updatedFieldsFile = [...values.fields]
+                                                updatedFieldsFile.splice(idx, 1)
+                                                setFieldValue("fields", updatedFieldsFile)
+                                            }}>
+                                            <i className="fa fa-trash"></i>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </table>
+            </div>
+
+            
         </>
     )
 }
--- frontend/src/pages/IntegrattionLayoutsNewEdit/IntegrattionLayouts.jsx
+++ frontend/src/pages/IntegrattionLayoutsNewEdit/IntegrattionLayouts.jsx
@@ -339,48 +339,17 @@ export default function IntegrattionLayouts({history}){
                                 <label className="col-form-label font-weight-600">Configuração dos Campos do Layout:</label>                                
                             </div>
 
-                            <div className="form row">
-                                <table className="table ml-3 table-striped table-bordered table-hover">
-                                    <thead>
-                                        <tr className="d-flex justify-content-center text-center">
-                                            <th className="col-4 fields-of-table align-center">Campo</th>
-                                            <th className="col-1 fields-of-table align-center">Posição Inicial</th>
-                                            <th className="col-1 fields-of-table align-center">Posição Final</th>
-                                            <th className="col-3 fields-of-table align-center">Nome Coluna</th>
-                                            <th className="col-1 fields-of-table align-center">Formato Data</th>
-                                            <th className="col-2 fields-of-table align-center">
-                                                <div className="font-weight-600">Ações</div>
-                                                <button className="btn btn-success btn-sm btn10px ml-3" type="button" style={{height:25}}
-                                                    onClick={() => {
-                                                        setFieldValue("fields", [...values.fields, { 
-                                                            nameField: "",
-                                                            positionInFile: "",
-                                                            positionInFileEnd: "",
-                                                            nameColumn: "",
-                                                            formatDate: ""
-                                                        }])
-                                                    } }>
-                                                    <i className="fa fa-plus"></i>
-                                                </button>
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>{
-                                        values.fields.map( (field, idx) => (
-                                            < IntegrattionLayoutsFieldsList
-                                                key={`fieldFile-${idx}`}
-                                                idx={idx}
-                                                fieldsFile={values.fields}
-                                                setFieldValue={setFieldValue}
-                                                initialValues={values.fields}
-                                                fileType={values.fileType}
-                                                valuesParent={values}
-                                            /> 
-                                        ))
-                                    }
-                                    </tbody>
-                                </table>
-                            </div>
+                            < IntegrattionLayoutsFieldsList
+                                values={values}
+                                errors={errors}
+                                touched={touched}
+                                handleChange={handleChange}
+                                handleBlur={handleBlur}
+                                setFieldValue={setFieldValue}
+                                setFieldTouched={setFieldTouched}
+                                defaultValues={defaultValues}
+                                initialValues={values}
+                            />
 
                             < IntegrattionLayoutsFieldsListValidation
                                 values={values}
