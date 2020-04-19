ed934aba7040da843b7ee81fa2cd005a4dc013eb
= <=>
2020-04-19 13:29:00 -0300

    Adicionado a base do IntegrattionCompanies

---
+++ backend/site/src/models/IntegrattionCompanies.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+// cria como se fosse a tabela do banco de dados este UserSchema
+const IntegrattionCompaniesSchema = new mongoose.Schema({
+    codi_emp: Number,
+    accountPaid: {
+        layouts: [{
+            idLayout: [mongoose.Schema.Types.ObjectId]
+        }]
+    }
+})
+
+// cria a 'tabela' de fato
+module.exports = mongoose.model('integrattion_companies', IntegrattionCompaniesSchema, 'IntegrattionCompanies')
