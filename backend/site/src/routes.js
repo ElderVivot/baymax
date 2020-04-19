d79470cc48d2584e4c5afaa4f2fdc49cd9a51387
= <=>
2020-04-19 13:53:29 -0300

    Implementado no routes o integrattion_companies

--- backend/site/src/routes.js
+++ backend/site/src/routes.js
@@ -1,15 +1,20 @@
 const express = require('express')
 
 const IntegrattionLayoutController = require('./controllers/IntegrattionLayoutController')
+const IntegrattionCompaniesController = require('./controllers/IntegrattionCompaniesController')
 
 // routes cria as rotas
 const routes = express.Router()
 
+// rotas do integrattion_layouts
 routes.get('/integrattion_layouts', IntegrattionLayoutController.index)
 routes.get('/integrattion_layouts/:id', IntegrattionLayoutController.show)
 routes.post('/integrattion_layouts', IntegrattionLayoutController.store)
 routes.put('/integrattion_layouts/:id', IntegrattionLayoutController.update)
 routes.delete('/integrattion_layouts/:id', IntegrattionLayoutController.delete)
+
+// rotas do integrattion_companies
+routes.post('/integrattion_companies', IntegrattionCompaniesController.store)
 
 // exportando as rotas
 module.exports = routes
