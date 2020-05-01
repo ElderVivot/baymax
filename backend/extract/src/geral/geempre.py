ac116b3d01b96258b503080aa2093c9476e97b09
= <=>
2020-05-01 10:21:22 -0300

    Implementado pra exportar apenas as colunas necessárias até agora

--- backend/extract/src/geral/geempre.py
+++ backend/extract/src/geral/geempre.py
@@ -38,18 +38,33 @@ class extractGeempre():
     def exportData(self):
         try:
             self._cursor = self._connection.cursor()
-            sql = ("SELECT * FROM bethadba.geempre ORDER BY codi_emp")
+            sql = ("  SELECT emp.codi_emp, emp.apel_emp, emp.razao_emp, emp.cgce_emp, emp.tins_emp, emp.stat_emp, emp.dcad_emp, emp.dina_emp,"
+                   "         emp.dtinicio_emp, emp.dddf_emp, emp.fone_emp, emp.email_emp, emp.i_cnae20, emp.ramo_emp, emp.rleg_emp, emp.esta_emp,"
+                   "         COALESCE( mun.nome_municipio, '' ) AS nome_municipio_emp,"
+                   "         COALESCE( vig.rfed_par, 0 ) AS regime_emp,"
+                   "         COALESCE( CASE WHEN regime_emp IS NULL THEN ''"
+                   "                        WHEN regime_emp IN (2,4) THEN vig.simplesn_regime_par"
+                   "                        ELSE federais_regime_par"
+                   "                   END, 'C' ) AS regime_caixa_emp"
+                   "    FROM bethadba.geempre AS emp"
+                   "         LEFT OUTER JOIN bethadba.gemunicipio AS mun"
+                   "                   ON    mun.codigo_municipio = emp.codigo_municipio"
+                   "         LEFT OUTER JOIN bethadba.efparametro_vigencia AS vig"
+                   "                   ON    vig.codi_emp = emp.codi_emp"
+                   "   WHERE vig.vigencia_par = ( SELECT MAX(vig2.vigencia_par)"
+                   "                                FROM bethadba.efparametro_vigencia AS vig2"
+                   "                               WHERE vig2.codi_emp = vig.codi_emp"
+                   "                                 AND vig2.vigencia_par <= today() )"
+                   "ORDER BY emp.codi_emp")
             self._cursor.execute(sql)
 
             df = pd.read_sql_query(sql, self._connection)
 
             data = json.loads(df.to_json(orient='records', date_format='iso'))
+            print('- Exportando empresas:')
             for companie in data:
-                existsCompanie = self._collection.find_one( { "codi_emp": companie['codi_emp'] } )
-                if existsCompanie is None:
-                    self._collection.insert_one(companie)
-                else:
-                    self._collection.update_one( { "codi_emp": companie['codi_emp'] }, { "$set": companie} )
+                self._collection.update_one( { "codi_emp": companie['codi_emp'] }, { "$set": companie}, upsert=True )
+                print(f"\t-{companie['codi_emp']} - {companie['razao_emp']}")
 
             df.to_json(self._wayToSave, orient='records', date_format='iso' )
         except Exception as e:
