979b9eadfdacdb77deb911085780df85c8594e46
= <=>
2020-05-05 07:11:41 -0300

    Implementado para que a URL seja composta de acordo o filtro passado

--- frontend/src/services/GetIntegrattionCompanies.js
+++ frontend/src/services/GetIntegrattionCompanies.js
@@ -1,14 +1,16 @@
 const { api } = require('./api')
+const util = require('../utils/util')
 
 class GetIntegrattionCompanies{
     constructor(filter={}){
         this.integrattion_companies = []
         this.filter = filter
+        this.url = util.implementsFilterInURL('/integrattion_companies', this.filter)
     }
 
     async getData(){
         try {
-            const responseIntegrattionCompanies = await api.get(`/integrattion_companies`)
+            const responseIntegrattionCompanies = await api.get(this.url)
             if(responseIntegrattionCompanies.statusText === "OK"){
                 this.integrattion_companies = responseIntegrattionCompanies.data
             }
--- frontend/src/services/GetIntegrattionLayouts.js
+++ frontend/src/services/GetIntegrattionLayouts.js
@@ -1,14 +1,16 @@
 const { api } = require('./api')
+const util = require('../utils/util')
 
 class GetIntegrattionLayouts{
     constructor(filter={}){
         this.integrattion_layouts = []
         this.filter = filter
+        this.url = util.implementsFilterInURL('/integrattion_layouts', this.filter)
     }
 
     async getData(){
         try {
-            const responseLayouts = await api.get(`/integrattion_layouts`)
+            const responseLayouts = await api.get(this.url)
             if(responseLayouts.statusText === "OK"){
                 this.integrattion_layouts = responseLayouts.data
             }
