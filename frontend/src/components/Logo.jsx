9358dbd8718c79cd4993c8c34fd7196b3d697c19
= <=>
2020-05-05 20:04:02 -0300

    Retirado pro Header e Logo ser fixo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default props =>
-    <aside className="logo d-flex align-items-center justify-content-center sticky-top">
+    <aside className="logo d-flex align-items-center justify-content-center">
         <span className="border border-white rounded p-1">
             <Link to="" className="logo">
                 <img src={logo} alt="logo" height="55px" width="55px"/>
