b716c0743b6b037f28121a058ad5e82b25d23dd6
= <=>
2020-05-05 23:55:50 -0300

    Adicionado a leitura das empresas modelo antigo

---
+++ backend/extract/src/geral/empresas_integrattion_old.py
@@ -0,0 +1,67 @@
+# coding: utf-8
+
+import os
+import sys
+
+absPath = os.path.dirname(os.path.abspath(__file__))
+fileDir = absPath[:absPath.find('backend')]
+sys.path.append(os.path.join(fileDir, 'backend/extract/src'))
+sys.path.append(os.path.join(fileDir, 'backend'))
+
+import json
+from datetime import datetime
+from dateutil import relativedelta
+from dao.src.ConnectMongo import ConnectMongo
+from extract.src.geral.geempre import ExtractGeempre
+from tools.leArquivos import leXls_Xlsx
+import tools.funcoesUteis as funcoesUteis
+
+class extractGeempre():
+    def __init__(self):
+        self._columns = []
+        self._geempre = ExtractGeempre()
+
+        self._connectionMongo = ConnectMongo()
+        self._dbMongo = self._connectionMongo.getConnetion()
+        self._collection = self._dbMongo['ExtractCompanies']
+    
+    def process(self):
+        try:
+            companiesDominio = self._geempre.getCompanies()
+
+            companiesExcel = leXls_Xlsx('C:/Programming/baymax/backend/extract/data/empresas_integrattion_old.xlsx', 'Já Avaliadas - Hist')
+
+            print('- Lendo dados das integrações antigas:')
+
+            for companie in companiesExcel:
+                codi_emp = funcoesUteis.treatNumberField(companie[0], isInt=True)
+                nome_emp = funcoesUteis.treatTextField(companie[1])
+                responsibleFinancialClientOld = funcoesUteis.treatTextField(companie[4])
+                obsAccountPaidOld = funcoesUteis.treatTextField(companie[8])
+                dateAccountPaidOld = funcoesUteis.treatTextField(companie[7])
+                layoutsAccountPaidOld = funcoesUteis.treatTextField(companie[9])
+                hasIntegrated = funcoesUteis.treatTextField(companie[10])
+
+                dataCompanie = {
+                    'codi_emp': codi_emp,
+                    'responsibleFinancialClientOld': responsibleFinancialClientOld,
+                    'obsAccountPaidOld': obsAccountPaidOld,
+                    'dateAccountPaidOld': dateAccountPaidOld,
+                    'layoutsAccountPaidOld': layoutsAccountPaidOld
+                }
+
+                existCompanieDominio = list(filter(lambda companieDominio: companieDominio['codi_emp'] == codi_emp, companiesDominio))
+                if len(existCompanieDominio) > 0 and hasIntegrated == "SIM":
+                    self._collection.update_one( { "codi_emp": codi_emp }, { "$set": dataCompanie}, upsert=True )
+                    print(f"\t - {codi_emp} - {nome_emp}")
+
+        except Exception as e:
+            print(f"O erro é: {e}")
+        finally:
+            self._connectionMongo.closeConnection()
+
+
+if __name__ == "__main__":
+    geempre = extractGeempre()
+    geempre.process()
+
