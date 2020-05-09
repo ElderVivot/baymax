6c7abd1703b1dc3d2434c435848cba83ceefd41f
= <=>
2020-05-09 20:21:06 -0300

    Adicionado a função que pode pegar o valor de um campo dentro de array ou objeto

--- frontend/src/utils/util.js
+++ frontend/src/utils/util.js
@@ -22,3 +22,35 @@ function implementsFilterInURL(baseURL='', filter={}){
     return url
 }
 module.exports.implementsFilterInURL = implementsFilterInURL
+
+function getDataInObjectOrArray(data, vector=[], defaultReturn=undefined){
+    try {
+        let dataReturn
+        if(vector.length === 1){
+            dataReturn = data[vector[0]]
+        } else if(vector.length === 2){
+            dataReturn = data[vector[0]][vector[1]]
+        } else if(vector.length === 3){
+            dataReturn = data[vector[0]][vector[1]][vector[2]]
+        } else if(vector.length === 4){
+            dataReturn = data[vector[0]][vector[1]][vector[2]][vector[3]]
+        } else if(vector.length === 5){
+            dataReturn = data[vector[0]][vector[1]][vector[2]][vector[3]][vector[4]]
+        } else if(vector.length === 6){
+            dataReturn = data[vector[0]][vector[1]][vector[2]][vector[3]][vector[4]][vector[5]]
+        } else if(vector.length === 7){
+            dataReturn = data[vector[0]][vector[1]][vector[2]][vector[3]][vector[4]][vector[5]][vector[6]]
+        } else {
+            dataReturn = defaultReturn
+        }
+
+        if( ( dataReturn === undefined || dataReturn === null ) && ( defaultReturn !== undefined && defaultReturn !== null) ){
+            return defaultReturn
+        } else {
+            return dataReturn
+        }
+    } catch (error) {
+        return defaultReturn
+    }
+}
+module.exports.getDataInObjectOrArray = getDataInObjectOrArray
