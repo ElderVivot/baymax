38006901fe61391c3ad76c465fb23ffe3af77922
= <=>
2020-05-09 20:20:08 -0300

    Adicionado o lookup pro CompaniesSettings, IntegrattionCompanies e IntegrattionLayouts

--- backend/site/src/controllers/CompaniesSettingsController.js
+++ backend/site/src/controllers/CompaniesSettingsController.js
@@ -4,12 +4,34 @@ const ExtractCompanies = require('../models/ExtractCompanies')
 module.exports = {
     async index(req, res){
         const companiesSettings = await ExtractCompanies.aggregate([
+            // {
+            //     "$match": {"codi_emp": 5}
+            // },
             {
                 "$lookup": {
                     "from": "CompaniesSettings",
                     "localField": "codi_emp",
                     "foreignField": "codi_emp",
                     "as": "companiesSettings"
+                }
+            }, 
+            // {
+            //     "$unwind": { "path": "$companiesSettings", "preserveNullAndEmptyArrays": true }
+            // },
+            {
+                "$lookup": {
+                    "from": "IntegrattionCompanies",
+                    "localField": "codi_emp",
+                    "foreignField": "codi_emp",
+                    "as": "integrattionCompanies"
+                }
+            },
+            {
+                "$lookup": {
+                    "from": "IntegrattionLayouts",
+                    "localField": "integrattionCompanies.0.accountPaid.layouts.0.idLayout",
+                    "foreignField": "_id",
+                    "as": "integrattionLayouts"
                 }
             }
         ])
