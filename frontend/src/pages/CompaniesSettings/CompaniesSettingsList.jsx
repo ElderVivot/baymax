fd35f60c85d86805644085404dbf4575cac1a014
= <=>
2020-05-10 19:46:08 -0300

    Adicionado os filtros e pra salvar quando alterar algum dado

--- frontend/src/pages/CompaniesSettings/CompaniesSettingsList.jsx
+++ frontend/src/pages/CompaniesSettings/CompaniesSettingsList.jsx
@@ -1,46 +1,63 @@
 import './styles.css'
 import React, { useEffect, useState, useRef } from 'react'
 import { HotTable } from '@handsontable/react'
-const { setIntervalAsync } = require('set-interval-async/dynamic')
+import 'handsontable/languages/pt-BR'
+// const { setIntervalAsync } = require('set-interval-async/dynamic')
 
 const columns = require('./SettingsColumns')
-const UnionIntegrattionAndExtractsCompanies = require('../../services/UnionIntegrattionAndExtractsCompanies')
+const SettingsCompaniesSettings = require('../../services/SettingsCompaniesSettings')
 const PostCompaniesSettings = require('../../services/PostCompaniesSettings')
 
 const CompaniesSettingsList = ( {history} ) => {
     const hotTableComponent = useRef(null)
-    const [actionUpdate, setActionUpdate] = useState(false)
     const [dataSheet, setDataSheet] = useState([])
     
     useEffect(() => {
         async function loadCompaniesSettings() {
             try {
-                const unionIntegrattionAndExtractsCompanies = new UnionIntegrattionAndExtractsCompanies()
+                const settingsCompaniesSettings = new SettingsCompaniesSettings()
 
-                const dataIntegrattionAndExtractsCompanies = await unionIntegrattionAndExtractsCompanies.process()
-                setDataSheet(dataIntegrattionAndExtractsCompanies)
-                
-                setActionUpdate(true)
+                const dataSettingsCompaniesSettings = await settingsCompaniesSettings.process()
+                setDataSheet(dataSettingsCompaniesSettings)
             } catch (error) {
                 console.log(error)
             }
         }
 
         loadCompaniesSettings()
-    }, [actionUpdate])
+    }, [])
 
-    setIntervalAsync( async () => {
-        try {
-            const postCompaniesSettings = new PostCompaniesSettings()
-            await postCompaniesSettings.process(hotTableComponent.current.hotInstance.getData())
-        } catch (error) {
-            console.log(error)
-        }
-    }, 200000)
+    // setIntervalAsync( async () => {
+    //     try {
+    //         const postCompaniesSettings = new PostCompaniesSettings()
+    //         await postCompaniesSettings.process(hotTableComponent.current.hotInstance.getData())
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }, 200000)
     
-    const handleChanges = () => {
+    async function handleChanges(changes){
         try {
-            console.log(hotTableComponent.current.hotInstance.getData())
+            for(let [row] of changes) {
+                const dataRow = hotTableComponent.current.hotInstance.getDataAtRow(row)
+                const postCompaniesSettings = new PostCompaniesSettings(dataRow)
+                await postCompaniesSettings.postData()
+            }
+        } catch (error) {
+            return
+        }
+    }
+
+    function handleAfterLoadData(initialLoad) {
+        try {
+            const filtersPlugin = hotTableComponent.current.hotInstance.getPlugin('filters')
+            console.log(filtersPlugin)
+            
+            filtersPlugin.addCondition(3, 'by_value', [['Ativa']]);
+            filtersPlugin.filter();
+
+            filtersPlugin.addCondition(0, 'by_value', [[1]]);
+            filtersPlugin.filter();
         } catch (error) {
             return
         }
@@ -50,21 +67,25 @@ const CompaniesSettingsList = ( {history} ) => {
         <main className="content card container-fluid pt-3">
             <HotTable
                 ref={hotTableComponent}
+                language={'pt-BR'}
                 settings={{
                     data: dataSheet,
                     rowHeaders: true,
                     colHeaders: true,
-                    // width: '100%',
+                    width: '100vw',
                     // height: 1000,
                     colWidths: [70, 270, 130, 70, 170, 130, 90, 100, 100, 60, 180, 180, 80, 200, 300, 170, 100, 180, 100, 150, 150, 150, 150, 70, 70, 70, 70, 70, 100, 60, 350],
                     // autoColumnSize: {syncLimit: 300},
-                    autoRowSize: {syncLimit: 5},
+                    autoRowSize: true,
                     autoWrapCol: true,
                     autoWrapRow: true,
                     // rowHeights: 23,
                     // columnHeaderHeight: 23,
                     rowHeaderWidth: 30,
-                    afterChange: () => handleChanges(),
+                    afterLoadData: (initialLoad) => handleAfterLoadData(initialLoad),
+                    // afterLoadData: (initialLoad) => console.log(initialLoad),
+                    afterInit: () => console.log('teste 2'),
+                    afterChange: (changes) => handleChanges(changes),
                     columns: columns,
                     dropdownMenu: true,
                     filters: true,
@@ -72,15 +93,14 @@ const CompaniesSettingsList = ( {history} ) => {
                     className: "htCenter htMiddle",
                     manualColumnResize: true,
                     manualRowResize: true,
-                    // contextMenu: contextMenu,
-                    // contextMenu: true,
+                    contextMenu: true,
                     hiddenColumns: {
                         columns: [2, 6, 7, 8, 19, 20, 24, 25, 30],
                         indicators: true
                     },
                     // fixedColumnsLeft: 2,
                     manualColumnFreeze: true,
-                    licenseKey: 'non-commercial-and-evaluation',
+                    licenseKey: 'non-commercial-and-evaluation'
                 }}
             />
         </main>
