f01d2ab06d1de192c40b7f455891d8406ee37ed0
= <=>
2020-05-10 17:49:59 -0300

    Implementado pra buscar os dados necessário, retirado a consulta independente do IntegrattionCompanies e Layout pra otimizar recursos. Alterado o nome também pq agora não vai ter mais Union

---
+++ frontend/src/services/SettingsCompaniesSettings.js
@@ -0,0 +1,60 @@
+const util = require('../utils/util')
+const SettingsCompanies = require('../utils/SettingsCompanies')
+const GetCompaniesSettings = require('./GetCompaniesSettings')
+const StatusIntegrattionOfCompanie = require('./StatusIntegrattionOfCompanie')
+
+class SettingsCompaniesSettings{
+    constructor(){
+        this.getCompaniesSettings = new GetCompaniesSettings()
+        this.extract_companies = []
+        this.dataSettingsCompaniesSettings = []
+    }
+
+    async process(){
+        this.extract_companies = await this.getCompaniesSettings.getData()
+
+        for( let companie of this.extract_companies ) {
+            const companiesSettings = util.getDataInObjectOrArray(companie, ['companiesSettings', 0], {})
+            const integrattionLayouts = util.getDataInObjectOrArray(companie, ['integrattionLayouts', 0], {})
+
+            let dataSettingsCompaniesSettings = {
+                codi_emp: companie.codi_emp,
+                nome_emp: companie.nome_emp,
+                cgce_emp: SettingsCompanies.formatCgceEmp(companie.tins_emp, companie.cgce_emp),
+                stat_emp: SettingsCompanies.statEmp(companie.stat_emp),
+                regime_emp: SettingsCompanies.regimeEmp(companie.regime_emp),
+                regime_caixa_emp: SettingsCompanies.regimeCaixaEmp(companie.regime_caixa_emp),
+                dcad_emp: util.transformToDate(companie.dcad_emp),
+                dina_emp: util.transformToDate(companie.dina_emp),
+                telefone_emp: companiesSettings.telefone_emp || util.handleTelefone(companie.dddf_emp, companie.fone_emp),
+                email_emp: companiesSettings.email_emp || companie.email_emp,
+                isCompanyBranch: SettingsCompanies.isCompanyBranch(companie.tins_emp, companie.cgce_emp),
+                layoutsAccountPaid: integrattionLayouts.system || companie.layoutsAccountPaidOld,
+                dateAccountPaid: companiesSettings.dateAccountPaid || companie.dateAccountPaidOld,
+                obsAccountPaid: companiesSettings.obsAccountPaid || companie.obsAccountPaidOld,
+                responsibleFinancialClient: companiesSettings.responsibleFinancialClient || companie.responsibleFinancialClientOld,
+                analystReceivedTraining: companiesSettings.analystReceivedTraining,
+                dateReceivedTraining: companiesSettings.dateReceivedTraining,
+                nome_municipio_emp: companie.nome_municipio_emp,
+                esta_emp: companie.esta_emp,
+                ramo_emp: companie.ramo_emp,
+                fiscalTeam: companie.grupos_fiscal,
+                accountingTeam: companie.grupos_contabil,
+                qtdEntryNotes: companie.entradas,
+                qtdOutputNotes: companie.saidas,
+                qtdServiceNotes: companie.servicos,
+                qtdLancManual: companie.lan_manual,
+                qtdLancImported: companie.lan_importado,
+                groupCompanie: companie.groupCompanie,
+                dateAccountPaidOld: companie.dateAccountPaidOld
+            }
+
+            const statusIntegrattionOfCompanie = new StatusIntegrattionOfCompanie(dataSettingsCompaniesSettings)
+            dataSettingsCompaniesSettings.statusAccountPaid = statusIntegrattionOfCompanie.identifiesTheStatus()
+
+            this.dataSettingsCompaniesSettings.push(dataSettingsCompaniesSettings)
+        }
+        return this.dataSettingsCompaniesSettings
+    }
+}
+module.exports = SettingsCompaniesSettings
