32e228a2939398d2653e97db0d149668052e72b1
= <=>
2020-06-02 01:09:40 -0300

    Melhorado os comentários e implementado pra ser 10 minutos o tempo máximos de Listar os xmls e depois carregá-lo

--- backend/robot/src/nfsegoiania/ClickListarXML.js
+++ backend/robot/src/nfsegoiania/ClickListarXML.js
@@ -7,7 +7,7 @@ const ClickListarXML = async(page, newPagePromise, settingsProcessing) => {
         await frame.waitFor('[value=Listar]')
         await frame.click('[value=Listar]')
         const popup = await newPagePromise;
-        await page.goto(popup.url(), {waitUntil: 'load'})
+        await page.goto(popup.url(), {waitUntil: 'load', timeout: 600000}) // aguarda até 10 minutos carregar a página pra fazer o download
     } catch (error) {
         console.log('\t[Final-Empresa] - Erro ao listar os XMLs')
         console.log('\t-------------------------------------------------')
